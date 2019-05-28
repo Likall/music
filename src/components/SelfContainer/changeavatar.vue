631cc058bd0050acee1ab95e3942048a5bbc28bc
yangling <1286102267@qq.com>
2019-05-28 17:17:12 +0800

    个人中心完成

---
+++ src/components/SelfContainer/changeavatar.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="changebg">
+        <div class="change-container">
+            <div class="change-head">
+                <router-link to="/selfcenter/editselfinfo">个人设置</router-link>
+                <span> ></span>
+                <span>更换头像</span>
+            </div>
+            <div class="changecontent-container">
+                <div class="change-top-container">
+                    <input type="file" class="upload">
+                    <input type="button" value="上传头像">
+                    <span>支持jpg、png、bmp格式的图片，且文件小于20M</span>
+                </div>
+                <div class="change-middle-container">
+                    <div class="middle-left">
+                        <img src="../../img/selfcenter/upload.png" alt="">
+                    </div>
+                    <div class="middle-right">
+                        <span>头像预览</span>
+                        <div class="big-avatar">
+                            <img src="http://p1.music.126.net/RLeBJe4D1ZzUtltxfoKDMg==/109951163250239066.jpg?param=180y180" alt="">
+                            <p>大尺寸头像（180x180像素）</p>
+                        </div>
+                        <div class="small-avatar">
+                            <img src="http://p1.music.126.net/RLeBJe4D1ZzUtltxfoKDMg==/109951163250239066.jpg?param=180y180" alt="">
+                            <p>小尺寸头像（40x40像素）</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="change-bottom-container">
+                    <input type="button" value="保存" disabled="disabled">
+                    <input type="button" value="取消">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style type="text/css" scoped>
+    *{
+        padding: 0;
+        margin: 0;
+
+    }
+    .changebg{
+        background-color: rgb(245,245,245);
+    }
+    .change-container {
+        background-color: white;
+        width: 980px;
+        margin: 0 auto;
+        border: 1px lightgray solid;
+        padding: 40px;
+    }
+    .change-head{
+        border-bottom: 2px #d13030 solid;
+        padding-bottom: 10px;
+    }
+    .change-head a:nth-child(1){
+        color: #0c73c2;
+    }
+    a{
+        color: black;
+    }
+    .change-head span:nth-child(2){
+        text-decoration: none;
+
+    }
+    .change-top-container{
+        padding: 26px 0px 10px 0px;
+    }
+    .change-top-container input{
+        width: 95px;
+        height: 31px;
+        border-radius: 5px;
+        border: 1px lightgray solid;
+        font-size: 12px;
+        background:  linear-gradient(to bottom,rgb(255,255,255),rgb(241,241,241));
+    }
+    .change-top-container span {
+        font-size: 12px;
+        color: gray;
+        margin-left: 20px;
+    }
+    .upload{
+        display: none;
+    }
+    .change-middle-container{
+        display: flex;
+        margin-top: 20px;
+    }
+    .middle-left{
+        border-right: 1px lightgray solid;
+        padding-right: 40px;
+    }
+    .middle-left img{
+        border: 1px lightgray solid;
+        width: 322px;
+        height: 322px;
+    }
+    .middle-right{
+        padding-left: 40px;
+    }
+    .middle-right span{
+        display: block;
+        font-size: 12px;
+    }
+    .small-avatar{
+        margin-top: 20px;
+    }
+    .big-avatar >img {
+        width: 180px;
+        height: 180px;
+        margin-top: 20px;
+    }
+    .small-avatar >img{
+        width: 40px;
+        height: 40px;
+    }
+    .middle-right p{
+        font-size: 12px;
+        color: gray;
+        margin-top: 10px;
+    }
+    .change-bottom-container{
+        margin-top: 40px;
+        padding-bottom: 80px;
+    }
+    .change-bottom-container input{
+        width: 80px;
+        height: 31px;
+        background: linear-gradient(to bottom,rgb(255,255,255),rgb(241,241,241));
+        border: 1px lightgray solid;
+        border-radius: 5px;
+        margin-left: 20px;
+
+    }
+
+</style>
--- src/components/SelfContainer/editselfinfo.vue
+++ src/components/SelfContainer/editselfinfo.vue
@@ -1,7 +1,83 @@
 <template>
-    <div class="editselfinfo">
-        111
+    <div class="edtibg">
+        <div class="editselfinfo">
+            <div class="head-container">
+                <h3>个人设置</h3>
+            </div>
+            <div class="info-container">
+                <div class="left-container">
+                    <div class="item">
+                        <label class="lab">昵称:</label>
+                        <input type="text" class="input-info">
+                    </div>
+                    <div class="item item-introduce">
+                        <label class="lab introduce-lab">介绍:</label>
+                        <textarea class="tarea" ></textarea>
+                        <span>300</span>
+                    </div>
+                    <div class="item sex">
+                        <label class="lab ">性别:</label>
+                        <input type="radio"value="men">男
+                        <input type="radio"value="women">女
+                        <input type="radio"value="screat">保密
+                    </div>
+                    <div class="item select-container">
+                        <label class="lab">生日:</label>
+                        <select name="" >
+                            <option value="">1991</option>
+                            <option value="">1992</option>
+                            <option value="">1993</option>
+                            <option value="">1994</option>
+                            <option value="">1995</option>
+                        </select>年
+                        <select name="" >
+                            <option value="">1991</option>
+                            <option value="">1992</option>
+                            <option value="">1993</option>
+                            <option value="">1994</option>
+                            <option value="">1995</option>
+                        </select>月
+                        <select name="" >
+                            <option value="">1991</option>
+                            <option value="">1992</option>
+                            <option value="">1993</option>
+                            <option value="">1994</option>
+                            <option value="">1995</option>
+                        </select>日
+                    </div>
+                    <div class="item select-container">
+                        <label class="lab">地区:</label>
+                        <select name="" >
+                            <option value="">直辖市</option>
+                            <option value="">1992</option>
+                            <option value="">1993</option>
+                            <option value="">1994</option>
+                            <option value="">1995</option>
+                        </select>
+                        <select name="" >
+                            <option value="">北京市</option>
+                            <option value="">1992</option>
+                            <option value="">1993</option>
+                            <option value="">1994</option>
+                            <option value="">1995</option>
+                        </select>
+
+                    </div>
+                    <div class="item btn-container">
+                        <button class="btn-save">保存</button>
+                    </div>
+
+                </div>
+                <div class="right-container">
+                    <img src="http://p2.music.126.net/RLeBJe4D1ZzUtltxfoKDMg==/109951163250239066.jpg?param=140y140" alt="">
+                    <div class="right-bottom">
+                        <router-link to="/selfcenter/editselfinfo/changeavatar" >更换头像</router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
 </template>
 <script>
 
@@ -11,15 +87,116 @@
     *{
         padding: 0;
         margin: 0;
+
     }
-    body{
-        background-color: rgb(245,245,245);
-    }
+   .edtibg{
+       background-color: rgb(245,245,245);
+   }
     .editselfinfo{
         width: 980px;
         background-color: white;
-        height: 800px;
-        border: 2px red solid;
+        border: 1px lightgray solid;
+        margin: 0 auto;
+        padding: 40px;
 
+    }
+    .head-container{
+        border-bottom: 2px #d13030 solid;
+        padding-bottom: 10px;
+    }
+    .info-container{
+        margin-top: 30px;
+        display: flex;
+
+
+    }
+    .input-info{
+        width: 292px;
+    }
+    .lab{
+        line-height: 32px;
+        text-align: right;
+        width: 38px;
+        font-size: 14px;
+        color: gray;
+    }
+    .left-container{
+        width: 340px;
+
+    }
+    .right-container{
+        width: 140px;
+        height: 140px;
+        margin-left: 20px;
+        position: relative;
+    }
+    .item{
+        width: 340px;
+        margin-top: 20px;
+    }
+    .tarea{
+        height: 122px;
+        width: 292px;
+    }
+    .introduce-lab{
+        vertical-align: top;
+    }
+    .item-introduce{
+        position: relative;
+    }
+    .item-introduce span{
+        position: absolute;
+        bottom: 8px;
+        right: 15px;
+        font-size: 10px;
+        color: gray;
+    }
+   .item select{
+        margin-left: 20px;
+    }
+    .select-container{
+        font-size: 12px;
+        color: gray;
+    }
+    .btn-save{
+        width: 80px;
+        height: 31px;
+        border-radius: 5px;
+        background-color: #3988d3;
+        color: white;
+        border: 0px;
+        ;
+    }
+    .btn-container{
+        margin-top: 40px;
+        padding-left:50px
+    }
+    .sex{
+        font-size: 12px;
+        color: gray;
+    }
+    .sex input{
+        margin-left: 10px;
+
+    }
+    .right-container img{
+        height: 100%;
+        width: 100%;
+    }
+    .right-bottom{
+        height: 26px;
+        width: 140px;
+        background-color: #8e8e8e;
+        position: absolute;
+        bottom: 0;
+
+        text-align: center;
+        font-size: 12px;
+        line-height: 26px;
+        opacity: 0.8;
+    }
+    .right-bottom a{
+        color: #fff;
+        text-decoration: none;
     }
 </style>
